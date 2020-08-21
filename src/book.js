388bcdf4eee80d6a22a201ea1d03cf3104b20a73
Ismail Hartley <Ismailhartley1@gmail.com>
2020-08-20 19:29:21 -0600

    Add warm ups

--- src/book.js
+++ src/book.js
@@ -1,9 +1,20 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+
 }
 
+function buildMainCharacter(name)
+
+return
+
+
+var  = (Vassya")
+
 module.exports = {
+
   createTitle: createTitle,
+
+
   // buildMainCharacter: buildMainCharacter,
   // saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
