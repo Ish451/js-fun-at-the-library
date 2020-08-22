dab67a016aa294d2c38d2c6239640b7ef2e3ebd7
Ismail Hartley <Ismailhartley1@gmail.com>
2020-08-22 13:45:19 -0600

    Finished books

--- src/book.js
+++ src/book.js
@@ -1,23 +1,57 @@
 function createTitle(title) {
   return `The ${title}`;
 
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+};
+
+function saveReview(newReview, bookReviews) {
+  if (bookReviews.includes(newReview) === true) {
+    return bookReviews;
+  }
+  return bookReviews.push(newReview);
+
+
+};
+
+function calculatePageCount(bookTitle){
+  return bookTitle.length * 20
 }
 
-function buildMainCharacter(name)
+function writeBook(bookTitle, mainCharacter, pageCount, genre){
+  return{
 
-return
+    title: bookTitle,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre:"fantasy"
 
 
-var  = (Vassya")
+  }
+}
 
-module.exports = {
+
+
+
+
+function editBook(ghoulBook){
+  ghoulBook.pageCount = ghoulBook.pageCount * .75;
+}
+
+
+ module.exports = {
+
 
   createTitle: createTitle,
-
-
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
-}
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
+};
